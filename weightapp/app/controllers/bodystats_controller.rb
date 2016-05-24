fe4a38c81fee274f809dfdc3efa0b82bd2c604a9
aiden <aidenscott12@googlemail.com>
2016-05-24 08:06:48 +0100

    now able to get the stats out of the body of the message as an array and iterate through them

--- weightapp/app/controllers/bodystats_controller.rb
+++ weightapp/app/controllers/bodystats_controller.rb
@@ -11,35 +11,38 @@ protect_from_forgery :except => [:create, :mailgun_create]
 	end
 
   def mailgun_create
-    logger.debug :hello
-    logger.debug params['body-plain']
+   
+   # logger.debug params['body-plain']
     
-    data = params['body-plain'].split(":",2)
-
-        case data[0]
-          when "Time"
-            date = get_date(data[1])
-            puts date.to_s
-            statHash[:bodystat][:date] = date.to_s
-          when "Weight"
-            statHash[:bodystat][:body_weight] = data[1].tr("kg", '')
-          when "Body Water"
-            statHash[:bodystat][:body_water] = data[1].tr("%", '')
-          when "Body Fat"
-            statHash[:bodystat][:body_fat] = data[1].tr("%", '')
-          when "Bone"
-            statHash[:bodystat][:bone_weight] = data[1].tr("%", '')
-          when "BMI"
-            statHash[:bodystat][:bmi] = data[1].tr("%", '')
-          when "Visceral Fat"
-            statHash[:bodystat][:visceral_fat] = data[1].tr("%", '')
-          when "BMR"
-            statHash[:bodystat][:bmr] = data[1].tr(" kcal", '')
-          when "Muscle Mass"
-            statHash[:bodystat][:muscle_mass] = data[1].tr("%", '')
+    data = params['body-plain'].split("\r\n")
+    logger.debug data
+        #case data[0]
+          #when "Time"
+            #date = get_date(data[1])
+            #puts date.to_s
+            #statHash[:bodystat][:date] = date.to_s
+          #when "Weight"
+            #statHash[:bodystat][:body_weight] = data[1].tr("kg", '')
+          #when "Body Water"
+            #statHash[:bodystat][:body_water] = data[1].tr("%", '')
+          #when "Body Fat"
+            #statHash[:bodystat][:body_fat] = data[1].tr("%", '')
+          #when "Bone"
+            #statHash[:bodystat][:bone_weight] = data[1].tr("%", '')
+          #when "BMI"
+            #statHash[:bodystat][:bmi] = data[1].tr("%", '')
+          #when "Visceral Fat"
+            #statHash[:bodystat][:visceral_fat] = data[1].tr("%", '')
+          #when "BMR"
+            #statHash[:bodystat][:bmr] = data[1].tr(" kcal", '')
+          #when "Muscle Mass"
+            #statHash[:bodystat][:muscle_mass] = data[1].tr("%", '')
+        #end
+        data.shift
+        data.each do | stat |
+         logger.debug stat 
         end
- 
-        render plain: stats
+        render plain: statHash
 
   end
 
@@ -56,6 +59,7 @@ protect_from_forgery :except => [:create, :mailgun_create]
 			params.require(:bodystat).permit(
 				:date,
 				:body_weight,
+
 				:body_water,
 				:body_fat,
 				:bone_weight,
