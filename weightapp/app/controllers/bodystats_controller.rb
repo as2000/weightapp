601d6fa38fb0c2e8093d62590842607dee43bc42
aiden <aidenscott12@googlemail.com>
2016-06-13 16:24:55 +0100

    email import working

--- weightapp/app/controllers/bodystats_controller.rb
+++ weightapp/app/controllers/bodystats_controller.rb
@@ -1,78 +1,63 @@
 class BodystatsController < ApplicationController
-protect_from_forgery :except => [:create, :mailgun_create]	
+  require 'date'
+  protect_from_forgery :except => [:create, :mailgun_create]
 
   def index
         @bodystats = Bodystat.order(date: :asc)
- 
+
   end
 
 	def create
 		render plain: "ok"
-		date = DateTime.parse(params[:bodystat][:date])
+		date = DateTime.parse(params[:date])
 		@bodystat = Bodystat.new(bodystat_params)
 		@bodystat.date = date
 		@bodystat.save
 	end
 
   def mailgun_create
-   
-   # logger.debug params['body-plain']
-    
     data = params['body-plain'].split("\r\n")
     #logger.debug data
     statHash = Hash.new
-    statHash[:bodystat] = Hash.new
+    #statHash = Hash.new
     data.shift
     data.each do | stat |
-      stat = stat.split(":")
-      logger.debug stat 
-      case stat[0] 
-        #when "Time"
-          #logger.debug "in time"
-          #date = get_date(stat)
-          #puts date.to_s
-          #statHash[:bodystat][:date] = date.to_s
+      stat = stat.split(":", 2)
+      logger.debug stat
+      case stat[0]
+        when "Time"
+          time = stat[1].split(", ").first
+          date = stat[1].split(", ").last
+          dt = date +" " + time
+          ts = DateTime.strptime(dt,"%m/%d/%Y %R")
+          statHash[:date] = ts
         when "Weight"
-          statHash[:bodystat][:body_weight] = stat[1].tr("kg", '')
-           logger.debug "in weight"
+          statHash[:body_weight] = stat[1].tr("kg", '')
         when "Body Water"
-          statHash[:bodystat][:body_water] = stat[1].tr("%", '')
+          statHash[:body_water] = stat[1].tr("%", '')
         when "Body Fat"
-          statHash[:bodystat][:body_fat] = stat[1].tr("%", '')
+          statHash[:body_fat] = stat[1].tr("%", '')
         when "Bone"
-          statHash[:bodystat][:bone_weight] = stat[1].tr("%", '')
+          statHash[:bone_weight] = stat[1].tr("%", '')
         when "BMI"
-          statHash[:bodystat][:bmi] = stat[1].tr("%", '')
+          statHash[:bmi] = stat[1].tr("%", '')
         when "Visceral Fat"
-          statHash[:bodystat][:visceral_fat] = stat[1].tr("%", '')
+          statHash[:visceral_fat] = stat[1].tr("%", '')
         when "BMR"
-          statHash[:bodystat][:bmr] = stat[1].tr(" kcal", '')
+          statHash[:bmr] = stat[1].tr(" kcal", '')
         when "Muscle Mass"
-          statHash[:bodystat][:muscle_mass] = stat[1].tr("%", '')
+          statHash[:muscle_mass] = stat[1].tr("%", '')
       end
     end
-    logger.debug "out case "
-    logger.debug statHash[:bodystat]
-
-    statHash.each do |stat|
-      logger.debug stat
-    end
-
-  #date = DateTime.parse(statHash[:bodystat][:date])
-  #@bodystat = Bodystat.new(bodystat_params)
-  #@bodystat.date = date
-  #@bodystat.save
-
-
-
-    render plain: statHash 
-
+    @bodystat = Bodystat.new(statHash)
+    @bodystat.save
+    render plain: "200"
   end
 
 	def destroy
 	  @bodystat = Bodystat.find(params[:id])
 	  @bodystat.destroy
-	 
+
 	  redirect_to bodystats_path
 	end
 
--- weightapp/app/controllers/imports_controller.rb
+++ weightapp/app/controllers/imports_controller.rb
@@ -1,32 +1,7 @@
 class ImportsController < ApplicationController
   before_action :set_import, only: [:show, :edit, :update, :destroy]
 
-  skip_before_filter :verify_authenticity_token  
-  # GET /imports
-  # GET /imports.json
-  def index
-   # @imports = Import.all
-    render plain: :hello
-  end
-
-  def
-    parameters /Users:
-    
-  end
-
-  # GET /imports/1
-  # GET /imports/1.json
-  def show
-  end
-
-  # GET /imports/new
-  def new
-    logger.debug params 
-  end
-
-  # GET /imports/1/edit
-  def edit
-  end
+  skip_before_filter :verify_authenticity_token
 
   # POST /imports
   # POST /imports.json
@@ -37,30 +12,7 @@ class ImportsController < ApplicationController
     render plain: :thanks
   end
 
-  # PATCH/PUT /imports/1
-  # PATCH/PUT /imports/1.json
-  def update
-    respond_to do |format|
-      if @import.update(import_params)
-        format.html { redirect_to @import, notice: 'Import was successfully updated.' }
-        format.json { render :show, status: :ok, location: @import }
-      else
-        format.html { render :edit }
-        format.json { render json: @import.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /imports/1
-  # DELETE /imports/1.json
-  def destroy
-    @import.destroy
-    respond_to do |format|
-      format.html { redirect_to imports_url, notice: 'Import was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_import
