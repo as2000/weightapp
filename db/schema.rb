460966a7f5d1fcce654a79252766a20a46ce203b
aiden <aidenscott12@googlemail.com>
2016-08-03 12:33:43 +0100

    run adduser Migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424164843) do
+ActiveRecord::Schema.define(version: 20160803112637) do
 
   create_table "bodystats", force: :cascade do |t|
     t.datetime "date"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20160424164843) do
     t.decimal  "bmr"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
