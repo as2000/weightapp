afe5ab7367ba9102cbbc3e5d2344d6656f232164
aidenscott12@gmail.com <aiden2@aiden-s.local>
2016-10-10 19:27:27 +0100

    messing with page title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_title = "")
+    base_title = "page"
+    if page_title.empty?
+      "unnamed #{base_title}"
+    else 
+      "#{page_title} - #{base_title}"
+    end
+  end
+
 end
