4ef0486cb573fde363b4d3511bd633c5671f5384
aidenscott12@gmail.com <aiden2@aiden-s.local>
2016-10-13 08:10:13 +0100

    signup page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :charts
   root 'list#index'
   post '/mailgun/', to: 'bodystats#mailgun_create'
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
