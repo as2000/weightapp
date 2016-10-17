f26ebb14f37e07ccad977afa72b4687fcfba6ac1
aidenscott12@gmail.com <aiden2@aiden-s.local>
2016-10-17 20:22:11 +0100

    mesage in flash scope and redirect to user on successfull signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render plain:'ok'
+      flash[:success] = 'welcome to the appp'
+      redirect_to @user
     else
       render 'new' 
     end
